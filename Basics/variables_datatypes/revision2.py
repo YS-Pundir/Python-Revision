40b9e1682310161162791966d5fc32463d701cc9
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-18 22:13:08 +0100

    doing some string operations

---
+++ Basics/variables_datatypes/revision2.py
@@ -0,0 +1,12 @@
+# Taking string from user
+str=input("enter a string :",)
+#printing the string enetred by  the user
+print("the string entered by the user is :",str)
+#now i am priting first chararcter of the string 
+print("the first element of the string is ",str[0])
+#printing the last character of the string
+print("the last element of the string is : ",str[-1])
+#now i am spliting the string 
+print("the sliced string from index 0 to 4 is :",str[0:4])
+
+#in this program i have practiced diffrent string operation
