3ee9ff9bd0dce0f51b56d9b0f52db7594dca1498
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-18 22:38:15 +0100

    additional operation on string in file revision3.py

---
+++ Basics/variables_datatypes/revision3.py
@@ -0,0 +1,15 @@
+str=input("enter a string :",)
+str2=input("enter another  string : ",)
+#First printing the original string enetred by the user
+print("the string entered by the user is :",str)
+#first ttring addition using + operator
+str_add=str+str2
+print("the addtion of these two string is :",str_add)
+# + this operator is used to concatenate two strings
+#now i am tring to use *operator on these strings
+str_mul=str*2
+print("the multiplication of these two string is :",str_mul)
+#this operator is used to repeat the string for certain number of times
+
+# In this program i have practiced diffrent string operation using two strings entered by the user
+
