a7fc01731a085004831460dff719479884a3d204
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-18 21:52:29 +0100

    applied arithematic operations in file revision1.py

---
+++ Basics/variables_datatypes/revision1.py
@@ -0,0 +1,24 @@
+a=int(input("Enter first number: "),)
+b=int(input("Enter second number: "),)
+sum=a+b
+print("The sum of",a,"and",b,"is",sum)
+# This program takes two integer inputs from the user and calculates their sum.
+# It then prints the results in a formatted string.
+product=a*b
+print("the product of the two numbers is",product)
+#moreover it calculates the product of thhe two number and prints it in same manner.
+diff=a-b
+print("the diffrence of the two numbers is ",diff)
+#Also, it meassures the diffrence bitween the two numbers and execute it .
+division=a/b
+print("the divion of the entered numbers is ",division)
+#Finally, it divides the first number by the second and displays the result.
+modulus=a%b
+print("the modulus of the number , enetred by user is ",modulus)
+#It is used to calculate the remainder , when first number is divided by the second one.
+power=a**b
+print("a raise to the power b is ",power)
+#this operater highly used to find power of any number .
+
+#In  all these operation i practiced diffrent arithematic opertaion in python using two numbers enetred by the user
+#these are the operation that i should know as a programmer , on daily to daily  basis of life.
