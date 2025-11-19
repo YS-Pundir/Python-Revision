f161ab64894ec3754194621602d71315646c2df1
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-20 00:10:01 +0100

    Programe to check if the alphabet entered by user is vowel or Consonant

---
+++ Basics/Conditional_statements/exercise5.py
@@ -0,0 +1,7 @@
+# Programe to check if the alphabet entered by user is vowel or Consonant
+ch=input("enter an alphabet :",)
+print("the alphabet you entered is  :",ch)
+if(ch=="a" or ch=="e" or ch=="i" or ch=="0" or ch=="u" or ch=="A" or ch=="E" or ch=="I" or ch=="O" or ch=="U"):
+    print("the alphabet is a vowel .")
+else:
+    print("the alphabet is a conssonent .")
