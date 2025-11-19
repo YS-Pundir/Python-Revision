c12ca83915bc4a68e521161fdaed6f011c27c037
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-19 23:51:22 +0100

    Adding a programe to check eligiblity to vote

---
+++ Basics/Conditional_statements/Exercise3.py
@@ -0,0 +1,6 @@
+# - A program to check if a person is eligile to vote (at least 18 years old)
+Age=int(input("Enter your age :",))
+if(Age >=18):
+    print("you are  eligible to vote .")
+else:
+    print("Sorry , you need to be at leats 18 years old to vote .")
