4b984cd2543e566769beb3f576a365efd14abd6f
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-21 00:38:58 +0100

    Adding progarm to find product of n numbers using while loop

--- Basics/Loops/exercise2_2.py
+++ Basics/Loops/exercise2_2.py
@@ -1,1 +1,7 @@
 #Finding the product of first n numbers using while loop
+n=int(input("enter a number :",))
+product=1
+i=1
+while(i<=n):
+    product*=i
+print(" the product of first",n,"numbers is : ",product)
