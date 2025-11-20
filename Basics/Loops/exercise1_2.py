855e86eeb1d0ded5035ff7e442c4c69457d89437
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-21 00:27:19 +0100

    adding code to take input from user using function

---
+++ Basics/Loops/exercise1_2.py
@@ -0,0 +1,5 @@
+#This is second part of exercise1 , where while loop will be used instad of for loop
+#programe to find the ssum of any amount of numbers taken by users
+def command():
+    num=int(input("enter a number :",))
+    return num
