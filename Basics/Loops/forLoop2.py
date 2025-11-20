b5a7480cd2c973c1ddcdc1d24d11bacc749a6324
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-20 23:42:40 +0100

    Adding program to find sum bitween two numers , that were given by users

---
+++ Basics/Loops/forLoop2.py
@@ -0,0 +1,9 @@
+#program to find the sum between twwo numbers
+a=int(input("enter first number:",))
+b=int(input("enter second number:",))
+
+sum=0
+for i in range (a,b+1):
+    sum=sum+i
+
+print("the sum of numbers between ",a,"and ",b,"is : ",sum)
