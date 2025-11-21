ced254a45460acf6859e5d744c5d5deb818892d4
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-21 01:03:26 +0100

    adding better way to do that task

--- Basics/Loops/exercise4.py
+++ Basics/Loops/exercise4.py
@@ -2,6 +2,9 @@
 a=int(input("eneter first number : ",))
 b=int(input("enter second number :",))
 sum=0
-for i in range (a,b+1,2):
-    sum+=i
-print("sum of all even numbers between ",a,"and ",b,"is :",sum)
+for i in range (a,b+1):
+    if(i%2==0):
+        sum+=i
+    else:
+        continue
+print("the sum of all even numbers betwee ",a,"and ",b,"is :",sum)
