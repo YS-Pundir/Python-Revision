453f75dad5f31feddd740004c41e6bb4234fccec
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-23 04:00:54 +0100

    adding decreasing rright angledd triangle

---
+++ Basics/Loop_patterns/Basic3.py
@@ -0,0 +1,8 @@
+#opposite right angled triangle
+n=int(input("enter the number of rows : ",))
+for i in range(n):
+    for j in range(i,n):#for  eg : when i is 0, the n 5= total star will be 5
+        #when i is 1 , n is 5 = 5-1 is 4 , total star will be four ....and so on
+        print("@",end=" ")
+       
+    print("")
