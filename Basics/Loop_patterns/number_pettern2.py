5c420f23ef57cde1b38319899c2af7999e5c46cb
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-23 23:38:22 +0100

    adding another interesting pattern

---
+++ Basics/Loop_patterns/number_pettern2.py
@@ -0,0 +1,10 @@
+#pattern with left side decreaseing , but incrimenting the numbers by rows
+n=int(input("enter the number of rows : ",))
+for i  in range(n):
+    
+    for j in range(i+1):
+        print(i+1,end=" ")
+    print()
+        
+
+    
