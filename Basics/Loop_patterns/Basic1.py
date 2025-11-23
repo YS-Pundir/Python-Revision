7ce88dc0bab54b658b79a20baf6a47a821a3f990
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-23 03:47:26 +0100

    adding usefull comments and basic2.py file

--- Basics/Loop_patterns/Basic1.py
+++ Basics/Loop_patterns/Basic1.py
@@ -1,7 +1,7 @@
-#making a pattern
+#making a Square pattern
 
 n=int(input("entre the number of rows :  ",))
 for i in range(n):
     for j in range(n):
         print("#",end=" ")
-    print()
+    print()#To change line after every row , as first loop is risponsible for the the rows in patterns
---
+++ Basics/Loop_patterns/Basic2.py
@@ -0,0 +1,1 @@
+#adding right angled triangle
