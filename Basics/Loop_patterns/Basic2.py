8a739ec19deaff7e8890b6aa289c3e39e31390b8
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-23 03:52:53 +0100

    adding programe to add the right angled triangle

--- Basics/Loop_patterns/Basic2.py
+++ Basics/Loop_patterns/Basic2.py
@@ -1,1 +1,6 @@
 #adding right angled triangle
+n=int(input("enter the number of rows  : ",))
+for i in range(n):
+    for j in range(i+1):#"i+1", because the star in column is one more everytime then the no of row it is in 
+        print("$",end=" ")
+    print()
