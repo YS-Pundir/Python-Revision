f3d354feb1c2c38e003c3bccf1824c98b9402358
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 21:04:59 +0100

    reevising way to check the accurance fo any element in  a tuple

---
+++ Basics/ListsAndTuples/tuple.py
@@ -0,0 +1,5 @@
+#tuples are used when the changes should not be made with the elements later 
+tup=("30 december,2005","20 november,2004","14 octuber ,2003","20 january,2002")
+# checking the accurance of any number or element in the tuple 
+print("checking accurance of a number in a list : ",tup.count(1))
+
