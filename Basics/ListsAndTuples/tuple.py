7a4f14beed6c930d794edcd3023f81b9e5499856
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 21:24:12 +0100

    adding the programe to do some operation on tuples

--- Basics/ListsAndTuples/tuple.py
+++ Basics/ListsAndTuples/tuple.py
@@ -2,4 +2,5 @@
 tup=("30 december,2005","20 november,2004","14 octuber ,2003","20 january,2002")
 # checking the accurance of any number or element in the tuple 
 print("checking accurance of a number in a list : ",tup.count(1))
-
+#checking the index of a element of the tuple  , index always starts with 0
+print("the index of the element is : ",tup.index("20 november,2004"))
