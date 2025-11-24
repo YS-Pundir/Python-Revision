764c4004bcb8a90a417a22b2b07851a1926189c4
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 23:04:47 +0100

    Adding programe to find the percentage of the sudent from the dictionary and updating the contact details

--- Basics/dictionaryAndSet/DictionaryAssignment2.py
+++ Basics/dictionaryAndSet/DictionaryAssignment2.py
@@ -34,6 +34,38 @@ print(Students)
 #printing just the major subjects
 print("the major subjects being studied by a student : ",list(Students["Course"]["Subject"]["Major"].keys()))
 
+#now programe to find the percentage of the student 
+
+list1=[]#taking a blank list
+
+a=Students["Course"]["Subject"]["Major"]["Physics"]["Marks"]
+list1.append(a)
+b=Students["Course"]["Subject"]["Major"]["Chemistry"]["Marks"]
+list1.append(b)
+c=Students["Course"]["Subject"]["Major"]["Mathematics"]["Marks"]
+list1.append(c)
+d=Students["Course"]["Subject"]["Minor"]["Physical Education"]["Marks"]
+list1.append(d)
+e=Students["Course"]["Subject"]["Minor"]["English"]["Marks"]
+list1.append(e)
+#printin the result of appended list
+print("the list of marks are",list1)
+
+#adding all the marks
+sum=0
+for i in list1:
+    sum+=i
+#printing the percentage
+print("the percentage of marks being obtained by student is : ",(sum/500)*100)
+
+#updating the contact information
+Students["Contact Details"]["Phone Number"]=919896077777
+Students["Contact Details"]["Email Address"]="yuvrajpundir2555@gmail.com"
+print("The contact details of a student is ",Students["Contact Details"])
+
+    
+    
+
 
 
 
