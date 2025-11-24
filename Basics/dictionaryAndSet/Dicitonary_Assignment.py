4b60c2d2d267c12898588e9c1c8fb151eef9b3b3
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 21:45:47 +0100

    adding the programe to declare the name of ,location and inventory  of a book store

---
+++ Basics/dictionaryAndSet/Dicitonary_Assignment.py
@@ -0,0 +1,17 @@
+#Book store inventory management'
+Book_store={
+    "Name":"Ram Book Store",
+    "Location":"Berlin Freidrichtasse 14",
+    "inventory":{
+        "fiction":{
+            "1984":{"price":15.99,"stock":16,"Auther":"Gautham Manni"},
+            "Sex Education":{"price":17.6,"stock":30,"Auther":"Yuvraj Singh"}
+        },
+        "Non fiction":{
+            "Story of my Life":{"price":999,"Auther":"Annie bassant","Stock":"limited"},
+            "Experiment of my life":{"price":118,"Auther":"Gandhi","stock":18}
+
+        },
+    
+    }
+}
