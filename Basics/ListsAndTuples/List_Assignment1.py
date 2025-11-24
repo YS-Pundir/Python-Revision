b67d239877f226cb6a9f55ce84f4a8aced81c348
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 20:50:44 +0100

    adding the function to add the elements of list

---
+++ Basics/ListsAndTuples/List_Assignment1.py
@@ -0,0 +1,6 @@
+#taking elements of list from user and then doing some operations
+list=[]
+n=int(input("enter the length of the list : ",))
+for i in range (n):
+    num=int(input("enter the number : ",))
+    list.append(num)
