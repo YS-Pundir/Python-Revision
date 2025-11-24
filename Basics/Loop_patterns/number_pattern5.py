28db43cf64e24d04b1cbcec097c6be33939cb9d4
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 01:11:43 +0100

    pyramid with first incrementing and then decrementing in a single row

---
+++ Basics/Loop_patterns/number_pattern5.py
@@ -0,0 +1,16 @@
+#pyramid with first incrementing and then decrementing in a single row
+n=5
+for i in range(n):
+    p=1
+    for j in range(i,n):
+        print(" ",end=" ")
+        
+    for k in range(i):
+        print(p,end=" ")
+        p+=1
+        
+    
+    for l in range(i+1):
+        print(p,end=" ")
+        p-=1
+    print()
