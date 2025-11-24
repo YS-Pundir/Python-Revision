02fa47bb4613735dec3c6e06fa0961b0a40a973e
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 23:30:11 +0100

    adding a file with set initializationss and some basic but important operations

---
+++ Basics/dictionaryAndSet/set.py
@@ -0,0 +1,13 @@
+#creaating and undertanding sets
+set={1,2,3,4,5,6,6,7,7,"Yuvraj ","singh","Pundir"}
+#does not include the duplicate while printing the sets
+print(set)
+#adding an element in set
+set.add("Pushpa")
+print("the updated set is ",set)
+#to remove the sets
+set.remove("Yuvraj ")
+print("set with removed element is :",set)
+set2={1,4,5,6,10,11}
+set.update(set2)
+print("all updated set is here :",set)
