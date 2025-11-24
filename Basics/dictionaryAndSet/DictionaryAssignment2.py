e9f945ef43fd6f244248cf3a495fc297be83e2d1
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 22:19:51 +0100

    Adding a complete dictionary to manage the student info

---
+++ Basics/dictionaryAndSet/DictionaryAssignment2.py
@@ -0,0 +1,37 @@
+#making  a Student management system
+Students={
+    "Name":"Yuvraj Singh Pundir",
+    "Roll NUmber":39084376,
+    "Course":{
+        "Subject":{
+            "Major":{
+                "Physics":{"Teacher":"Rajesh Sharma","Marks":82},
+                "Chemistry":{"Teacher":"Pardeep","Marks":83},
+                "Mathematics":{"Teacher":"Amit Setia","Marks":93}
+            },
+            "Minor":{
+                "English":{"Teacher":"Dinesh Sharma","Marks":81},
+                "Physical Education":{"Teacher":"Sushma Mehla","Marks":99}
+
+            },
+
+        },
+        "Assessment":{
+            "Discipline":{"Marks":100},
+            "Punctuality":{"Marks":99}
+        },
+
+    },
+    "Parent Name":{
+    "Mother Name":"Mrs. Sudesh ","Father Name":"Mr. Satbir Singh"
+        
+        },
+    "Contact Details":{"Phone Number":+49177231111,"Address":"Berlin Alxenderplatz"}
+
+    },
+print(Students)
+
+
+
+
+
