4df89f92b6b9a20487b90a6770574492a3b421a5
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-24 20:51:41 +0100

    adding program to find average of elements of the list

--- Basics/ListsAndTuples/List_Assignment1.py
+++ Basics/ListsAndTuples/List_Assignment1.py
@@ -4,3 +4,15 @@ n=int(input("enter the length of the list : ",))
 for i in range (n):
     num=int(input("enter the number : ",))
     list.append(num)
+
+#function to print the sum of every element of list
+def additon():
+    sum=0
+    for i in list:
+        sum+=i
+    return sum
+
+
+    
+    
+    
