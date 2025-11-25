86c40f789f3264ee2e1579b2f1f9fcdf96d6c2f4
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-25 15:26:37 +0100

    adding programe to make function to swap to numbers taken by user

--- Basics/Functions/function2.py
+++ Basics/Functions/function2.py
@@ -1,3 +1,14 @@
 #function to swap two numbers
 a=int(input("enter the first number :",))
 b=int(input("enter the second number :",))
+
+#function to swwap the value of both numbers
+def swap(a,b):
+    temp=a
+    a=b
+    b=temp
+
+
+    print("the value of a now is :",a)
+    print(f"the value of b now is {b} ")
+swap(a,b)
