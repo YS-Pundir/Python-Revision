a71b66ba72755416ebe9bafd16574c426a7f89d2
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-25 15:18:50 +0100

    adding program to add a function to check if number given by user is odd or even

---
+++ Basics/Functions/function1.py
@@ -0,0 +1,11 @@
+#function to check if numbetr is even or odd
+n=int(input("enter the number :",))
+def check(n):
+    if(n%2==0):
+        print("the given number is even")
+        
+    else:
+        print("the number  is odd")
+        
+
+check()
