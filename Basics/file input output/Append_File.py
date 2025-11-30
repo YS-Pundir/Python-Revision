5b34ca63139ec1e7d426c50e146219309c39eadb
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-30 02:16:44 +0100

    programe to append thee text file

---
+++ Basics/file input output/Append_File.py
@@ -0,0 +1,5 @@
+#adending the file
+file=open("example.txt",'a')
+
+file.write("now i am appending file , which write the file without overwrighting the existing material\n so lets do it")
+file.close()
