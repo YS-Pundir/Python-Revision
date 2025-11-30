f3f39d623ee06604eb5a1d12ff121ee7e6cb046d
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-30 01:41:25 +0100

    adding programe to read a file line by line

--- Basics/file input output/file2.py
+++ Basics/file input output/file2.py
@@ -6,3 +6,11 @@ data=file.read()
 print(data)
 #always close the file 
 file.close()
+
+#way to read the line , always close the file to read the line of a file 
+#if we do not close the file  and try to read  it agian , there will nothing be  printed.
+file=open('example.txt','r')
+line1=file.readline()
+print(line1)
+line2=file.readline()
+print(line2)
