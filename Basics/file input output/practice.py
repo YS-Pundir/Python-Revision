546a04412d303303df77bcce291ba460c8f5679e
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-30 02:58:03 +0100

    writing progarme to creeate a file and add a desired text

---
+++ Basics/file input output/practice.py
@@ -0,0 +1,5 @@
+with open ("sample.txt","w") as f:
+    f.write("hi everyone \nwe are  learning file o/i \nusing java")
+    f.write("I like programing using Java")
+    f.close
+#replacing java with python
