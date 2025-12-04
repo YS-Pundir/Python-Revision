cf871f6d8ceec85fc18665d3f003acc34fda69fa
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-04 17:12:56 +0100

    adding the  method to find the area of other objects as well

--- Basics/OOPS/Shape_Hirarchy.py
+++ Basics/OOPS/Shape_Hirarchy.py
@@ -2,16 +2,36 @@ import math
 
 class shape():
     def area(self):
-
-        raise NotImplementedError("this mehtod to be over wright in subclasses")
+        return 0
+    
     
 class circle (shape):
     def __init__(self,r):
-        self.radius=r
+        self.r=r
 
     def area(self):
         return math.pi*self.r**2
     
+class rectangle(shape):
+    def __init__(self,length,breath):
+        self.length=length
+        self.breath=breath
+
+    def area(self):
+        return self.length*self.breath
+    
+class triangle (shape):
+    def __init__(self,length , breath,height):
+        self.length=length
+        self.breath=breath
+        self.height=height
+
+    def area(self):
+        return self.breath*self.height*self.length
+    
+shapes=[circle(3),rectangle(4,5),triangle(5,6,7)]
+for i in shapes:
+    print(f"the area of {i} is : {i.area()}")
 
         
 
