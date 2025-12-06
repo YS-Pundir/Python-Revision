db472b1bf8672d63e600afc93a62da4ec4de409d
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-06 11:12:08 +0100

    addding class Book to the librarySystem

--- Basics/OOPS/OnllineLibraraSystem.py
+++ Basics/OOPS/OnllineLibraraSystem.py
@@ -1,2 +1,10 @@
+class Book:
+    def __init__(self,Title,Auther,isbn,is_borrowed):
+        self.Title=Title
+        self.Auther=Auther
+        self.isbn=isbn
+        self.is_borrowed=is_borrowed
 
+Book1=Book("Experiment of my life","Gandhi",1234,True)
+print(Book1.is_borrowed)
 
