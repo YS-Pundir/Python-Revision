338bbbf26916a81d72e092b8bb3c4cbf8c1bece7
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-06 12:35:22 +0100

    adding method to add book in the libraries

--- Basics/OOPS/OnllineLibrarySystem.py
+++ Basics/OOPS/OnllineLibrarySystem.py
@@ -3,8 +3,50 @@ class Book:
         self.Title=Title
         self.Auther=Auther
         self.isbn=isbn
-        self.is_borrowed=is_borrowed
+        self.is_borrowed=False
 
-Book1=Book("Experiment of my life","Gandhi",1234,True)
-print(Book1.is_borrowed)
+class Library():
+    def __init__(self,name):
+        self.name=name
+        self.listOfBooks=[]
+
+    def addBooks(self):
+        maxLim=int(input("Add the maximum limit of book that can be added at once : ",))
+        print(f"the maximum limit of adding the book is {maxLim}")
+        print("---------Add Books---------")
+
+        for  i in range(maxLim): 
+            print("enter the details of the book that  needed to be added ->")
+            
+            Title=input("eneter the name of the book : ",)
+            Auther=input("enter the name of the auther : ",)
+            ISBN=input("enter the isbn of the book :",)
+
+            book=Book(Title,Auther,ISBN,False)
+            self.listOfBooks.append(book)
+            print(f"<><><>The Book {book.Title} has been added successfully <><><>")
+
+        n=1
+        print("Books in the libraries are the following -->")
+        for i in self.listOfBooks:
+            
+            print(f" {n}. {book.Title} by {book.Auther}")
+            n+=1
+
+            
+ 
+        
+
+
+lib=Library("City Library")
+lib.addBooks()
+
+
+
+
+
+        
+
+
+
 
