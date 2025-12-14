0f618f1747f04943e948859c0ea6e5d19a10c4f9
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-14 04:42:59 +0100

    adding  a bassic practice of matplot lib

--- Python_Libraries/matplot/matplot1.py
+++ Python_Libraries/matplot/matplot1.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
+
 x=[1,2,3,4]
 y=[10,20,15,25]
 plt.plot(x,y)
+plt.grid()
+plt.xlabel("X-axis",color='blue',labelpad=10)
+plt.ylabel("y-axis",color='blue',labelpad=10)
+plt.title("Simple Line Plot")
+plt.text(3,15,"#",fontsize=10,color='blue')
 plt.show()
