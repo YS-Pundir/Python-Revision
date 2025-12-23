373ed4a54d82f5337493b6594e0116a279a1a13e
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-23 22:25:09 +0100

    adding remove item and manage inventory funciton

--- Basics/Functions/iit_Question2.py
+++ Basics/Functions/iit_Question2.py
@@ -6,3 +6,30 @@ def add_item(inventory, item, quantity):
     else:
         inventory[item] =quantity
     return inventory
+
+def remove_item(inventory, item, quantity):
+    if item in inventory:
+        if inventory[item]>=quantity:
+            inventory[item]-=quantity
+        else:
+            inventory.pop(item)
+    else:
+        print(f"{item} not found in inventory.")
+    return inventory
+    
+def display_inventory(inventory):
+    for item,quantity in inventory.items():
+        print(f"{item}:{quantity}")
+
+def manage_inventory(inventory):
+    print("after adding items :")
+    inventory=add_item(inventory,"banana",10)
+    inventory=add_item(inventory,"grape",15)
+    print(inventory)
+    print("after removing items :")
+    inventory=remove_item(inventory,"apple",5)
+    inventory=remove_item(inventory,"orange",10)
+    print(inventory)
+    print("current inventory :")
+    display_inventory(inventory)
+manage_inventory(inventory)
