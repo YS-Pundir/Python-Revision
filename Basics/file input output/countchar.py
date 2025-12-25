cc55f7abda2b06ae7d8d8b2de1696984cb119780
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-26 00:42:08 +0100

    adding programe to add the function to count total  caharacters in the text

--- Basics/file input output/countchar.py
+++ Basics/file input output/countchar.py
@@ -1,4 +1,20 @@
 #total character in text
-with open("sample.txt",'r') as file:
-    text=file.read()
-    print(len(text))
+def  countfile():
+    name=input("enter the name of the file : ",)
+    try:
+        with open("name",'r') as file:
+            text=file.read()
+            print("Total number if charecter in the file is : ",len(text))
+    except:
+        print("file not found")
+def counttext():
+    text=print("enter the text need to be analysed : ",)
+    print("total number of charecter in text is : ",len(text))
+
+
+
+
+
+        
+        
+    
