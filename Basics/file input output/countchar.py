eca9ed69faf44a24a5e8648b00d397d65c40cd9f
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-26 02:10:06 +0100

    adding more function

--- Basics/file input output/countchar.py
+++ Basics/file input output/countchar.py
@@ -1,15 +1,39 @@
 #total character in text
-def  countfile():
-    name=input("enter the name of the file : ",)
-    try:
-        with open("name",'r') as file:
+
+
+def user_input():
+    line=[]
+    print("enter the input (press enter twice to finish): ")
+    while True:
+        words=input(" ",)
+        if words=="":
+            break
+        line.append(words)
+    text=" ".join(line)#join function joins the elements of data  structure with regular intervals
+    return text
+   
+    
+def file_input():
+    name=input("Enter the name of file to  be analysed : ",)
+    try :
+        with open("name","r") as file:
             text=file.read()
-            print("Total number if charecter in the file is : ",len(text))
-    except:
-        print("file not found")
-def counttext():
-    text=print("enter the text need to be analysed : ",)
-    print("total number of charecter in text is : ",len(text))
+    except :
+        print("file not found ")
+    return text
+
+
+
+def  count_char():
+
+    print("Total number of charecter in the file is : ",len(user_input()))
+
+count_char()
+
+
+
+    
+
 
 
 
