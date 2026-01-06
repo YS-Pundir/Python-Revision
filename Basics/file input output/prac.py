831265e1a3bf32d8ce45db5fdc0eceeec5911ca0
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-01-06 21:25:42 +0100

    adding programe to practice the csv writer

--- Basics/file input output/prac.py
+++ Basics/file input output/prac.py
@@ -1,5 +1,24 @@
 import csv
-with open("machine-readable-business-employment-data-sep-2025-quarter.csv","r") as f:
-    reader=csv.reader(f)#csv have diffrent method toi be readed properly , it can not be reded by normal method as txt file
-    for row in reader:
-        print(row)
+
+data=[
+    ["Yuvraj ","Singh","Pundir"],
+    [34,77,89],
+    [88,23,53]
+]
+try:
+    # with open("machine-readable-business-employment-data-sep-2025-quarter.csv","r") as f:
+    #     reader=csv.reader(f)#csv have diffrent method toi be readed properly , it can not be reded by normal method as txt file
+    #     for row in reader:
+    #         print(row)
+    #     f.close()
+
+    with open("machine-readable-business-employment-data-sep-2025-quarter.csv","w",newline="") as f:
+        writer=csv.writer(f)
+        writer=writer.writerows(data)
+        
+except FileNotFoundError:
+    print("file  does not exsist")
+except TypeError:
+    print("No argument provided !!")
+    
+
