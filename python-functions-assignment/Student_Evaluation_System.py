1ee90455d695d7ee11ca74241b16ee0fef596daf
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-02-05 20:47:08 +0100

    adding the main function to get the interactive eperience for the user

--- python-functions-assignment/Student_Evaluation_System.py
+++ python-functions-assignment/Student_Evaluation_System.py
@@ -1,5 +1,4 @@
-name=input("Enter the name of student : ",)
-list=[45,76,99,100,99.8,67]
+
 
 def greeting(name):
     return f"Hello {name} "
@@ -12,12 +11,33 @@ def result(list):
     return len(list),sum/len(list)
     
 
-no_of_subjects,average=result(list)
 
-def Pass_Fail_checker():
+
+def Pass_Fail_checker(average):
     if  average >50:
         return "Pass"
     else:
         return "Fail"
     
 
+def main():
+    name=input("Enter the name of student : ",)
+    list=[]
+    n=int(input("Enetr the no of subject : ",))
+    for i in range(n):
+        marks=int(input("Enter the marks in the subject : "))
+        list.append(marks)
+
+    
+
+    print(greeting(name))
+
+    no_of_subjects,average=result(list)
+
+    print(f"Subjects :{no_of_subjects}")
+    print(f"Average Score : {average}")
+
+    print("Reesult : ",Pass_Fail_checker(average))
+
+if __name__ ==main():
+    main()
