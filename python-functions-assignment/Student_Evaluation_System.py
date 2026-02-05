520f148efa6ffadf7513a4abf65dc13d9b27cd86
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-02-05 20:34:46 +0100

    adding function in file of assignment to return the no of subjects and average of marks thathave been earned

---
+++ python-functions-assignment/Student_Evaluation_System.py
@@ -0,0 +1,17 @@
+name=input("Enter the name of student : ",)
+list=[45,76,99,100,99.8,67]
+
+def greeting(name):
+    return f"Hello {name} "
+
+def result(list):
+    sum=0
+    for i in list:
+        sum+=i
+
+    return len(list),sum/len(list)
+    
+
+
+
+
