f812e9b007a9b113ddafbfd362c98d130e5a6496
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-02-06 19:41:00 +0100

    adding the rest required statements

--- Python_Data_Assignment/Student_Marks_Analyser.py
+++ Python_Data_Assignment/Student_Marks_Analyser.py
@@ -3,3 +3,7 @@ marks=[97,56,44,65,77,87,99,21,56]
 print("Marks obtain by student is : ",marks )
 print("First Three Marks : ",marks[0:3])
 print("Last Three Marks :  ",marks[-3:])
+
+print("Highest Marks : ",max(marks))
+print("Lowest Marks : ",min(marks))
+print("Average marks : ",sum(marks)/len(marks))
