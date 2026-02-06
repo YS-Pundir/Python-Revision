5c3daa48be25a4fc43a7bba7628566d9f099351a
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-02-06 20:27:03 +0100

    Added function to calculate the average score of eaach use

--- Python_Data_Assignment/User_Data_Processing_System.py
+++ Python_Data_Assignment/User_Data_Processing_System.py
@@ -1,24 +1,35 @@
 user1={
     "Name":"Yuvraj Singh",
-    "Scores":"99.9/100",
-    "Role":"AI Engineer"
+    "Score":[99,78,96,99],
+    "Role":["AI Engineer","Build Models","Deploy The Models"]
 }
 
 user2={
     "Name":"Himanshu Sharma",
-    "Scores":"78/100",
-    "Role":"Data Analyst",
+    "Score":[78,67,98,67],
+    "Role":["Data Analyst","Analyse Data","Story Telling"]
 }
 
 user3={
     "Name":"Himanshu Rana",
-    "Score":"66/100",
-    "Role":"It"
+    "Score":[66,88,33,98],
+    "Role":["It","Resolve IT Problems","Build IT Agile"]
 }
 
 user4={
     "Name":"Prince Dhull",
-    "Score":"88/100",
-    "Role":"Finance"
+    "Score":[88,66,99,67],
+    "Role":["Finance","Moniter the money","Make Strategies"]
 }
 list_of_users=[user1,user2,user3,user4]
+
+def avg_Score(list):
+    dict={ }
+    sum=0
+    for i in list :
+        for j in i["Score"]:
+            sum+=j
+        dict[f"Average Score of {i["Name"]}"]=sum/len(i["Score"])
+
+        
+    return dict
