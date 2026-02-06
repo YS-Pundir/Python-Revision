c377fd6642fc16fd1be6c300d8ab505590a75226
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-02-06 20:10:58 +0100

    addding programe to store the user data using the list and dictionary

---
+++ Python_Data_Assignment/User_Data_Processing_System.py
@@ -0,0 +1,24 @@
+user1={
+    "Name":"Yuvraj Singh",
+    "Scores":"99.9/100",
+    "Role":"AI Engineer"
+}
+
+user2={
+    "Name":"Himanshu Sharma",
+    "Scores":"78/100",
+    "Role":"Data Analyst",
+}
+
+user3={
+    "Name":"Himanshu Rana",
+    "Score":"66/100",
+    "Role":"It"
+}
+
+user4={
+    "Name":"Prince Dhull",
+    "Score":"88/100",
+    "Role":"Finance"
+}
+list_of_users=[user1,user2,user3,user4]
