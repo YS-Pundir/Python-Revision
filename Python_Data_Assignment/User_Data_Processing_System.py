3b99502e0908c70e4c5935c2e90db866f48ce64f
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-02-06 20:33:38 +0100

    adding the role checker function in the assignment file

--- Python_Data_Assignment/User_Data_Processing_System.py
+++ Python_Data_Assignment/User_Data_Processing_System.py
@@ -1,26 +1,26 @@
 user1={
     "Name":"Yuvraj Singh",
     "Score":[99,78,96,99],
-    "Role":["AI Engineer","Build Models","Deploy The Models"]
+    "Role":{"AI Engineer","Build Models","Deploy The Mode","Admin"}
 }
 
 user2={
     "Name":"Himanshu Sharma",
     "Score":[78,67,98,67],
-    "Role":["Data Analyst","Analyse Data","Story Telling"]
+    "Role":{"Data Analyst","Analyse Data","Story Telling"}
 }
 
 user3={
     "Name":"Himanshu Rana",
     "Score":[66,88,33,98],
-    "Role":["It","Resolve IT Problems","Build IT Agile"]
-}
+    "Role":{"It","Resolve IT Problems","Build IT Agile"
+}}
 
 user4={
     "Name":"Prince Dhull",
     "Score":[88,66,99,67],
-    "Role":["Finance","Moniter the money","Make Strategies"]
-}
+    "Role":{"Finance","Moniter the money","Make Strategies"}
+    }
 list_of_users=[user1,user2,user3,user4]
 
 def avg_Score(list):
@@ -33,3 +33,11 @@ def avg_Score(list):
 
         
     return dict
+
+def role_checker(role):
+    if "Admin" in role:
+        return True
+    else:
+        return False
+    
+
