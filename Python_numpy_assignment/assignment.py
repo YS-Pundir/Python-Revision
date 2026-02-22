00eacc3094fc3200de66784afa76e6dfd641c31b
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-02-22 21:22:24 +0100

    adding the final touch to the assignment

--- Python_numpy_assignment/assignment.py
+++ Python_numpy_assignment/assignment.py
@@ -1,61 +1,63 @@
 import numpy as np
 
-# settigna random seed for better reproduciblity
-np.random.seed(42)
+def main():
 
-#generating dataset as required in assignment using numpy
-#samples are number of rows
-samples=100
-#features are number of columns
-features=5
+    # settigna random seed for better reproduciblity
+    np.random.seed(42)
 
-dataset=np.random.random((samples,features))
+    #generating dataset as required in assignment using numpy
+    #samples are number of rows
+    samples=100
+    #features are number of columns
+    features=5
 
-# Calculating the mean of features in dataset
-nmean=np.mean(dataset,axis=0)
-# i used axis=0 because it will make compilar to read from up to downside in every column one by one
+    dataset=np.random.random((samples,features))
 
-# standard deviation calculation as asked in assignment 
-nstd=np.std(dataset,axis=0)
+    # Calculating the mean of features in dataset
+    nmean=np.mean(dataset,axis=0)
+    # i used axis=0 because it will make compilar to read from up to downside in every column one by one
 
-#Normalizing the dataset
-norm=(dataset-nmean)/nstd
+    # standard deviation calculation as asked in assignment 
+    nstd=np.std(dataset,axis=0)
 
-# psliting the dataset by 80 percent for training and the rest for testing 
-ind=int(0.8*samples)
+    #Normalizing the dataset
+    norm=(dataset-nmean)/nstd
 
-training_dataset=norm[:ind]
-testing_dataset=norm[ind:]
+    # psliting the dataset by 80 percent for training and the rest for testing 
+    ind=int(0.8*samples)
 
-print("Normalized dataset ")
-print(norm)
+    training_dataset=norm[:ind]
+    testing_dataset=norm[ind:]
 
-print("training dataset")
-print(training_dataset)
+    #modifing a value in normalized dataset
 
-training_dataset[0,0]=5
+    training_dataset[0,0]=5
 
 
 
-# printing the orignal shape of dataset 
-print("<>"*70)
+    # printing the orignal shape of dataset 
+    print("<>"*70)
 
-print("Original Shape of Dataset : ",dataset.shape)
-print("Mean shape:",nmean.shape)
-print("Training data shape:",training_dataset.shape)
-print("Test data shape:",testing_dataset.shape)
-print("-"*20,"Note: Modifying the slice affected the original array","-"*20)
+    print("Original Shape of Dataset : ",dataset.shape)
+    print("Mean shape:",nmean.shape)
+    print("Training data shape:",training_dataset.shape)
+    print("Test data shape:",testing_dataset.shape)
+    print("-"*20,"Note: Modifying the slice affected the original array","-"*20)
 
-print("<>"*70)
+    print("<>"*70)
 
-print("-"*20," modified Normalized dataset ","-"*20)
-print()
-print(norm)
-print()
-print()
-print("-"*20," modified training dataset","-"*20)
-print()
-print(training_dataset)
+    print("-"*20," modified Normalized dataset ","-"*20)
+    print()
+    print(norm)
+    print()
+    print()
+    print("-"*20," modified training dataset","-"*20)
+    print()
+    print(training_dataset)
+
+
+if __name__== "__main__" :
+    main()
 
 
 
