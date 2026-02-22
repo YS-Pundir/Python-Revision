e7b1e16e38d4e7d70454567c892e785fc24ae95e
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-02-22 20:54:57 +0100

    generating the dataset using random in numpy library

--- Python_numpy_assignment/assignment.py
+++ Python_numpy_assignment/assignment.py
@@ -1,3 +1,16 @@
+import numpy as np
+
+# settigna random seed for better reproduciblity
+np.random.seed(42)
+
+#generating dataset as required in assignment using numpy
+#samples are number of rows
+samples=100
+#features are number of columns
+features=5
+
+dataset=np.random.random(samples,features)
+
 
 
 
