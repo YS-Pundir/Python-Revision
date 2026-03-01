5debda65dd9155e4aface7bbd5f8b3a7c02bb06c
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-03-01 18:46:33 +0100

    adding a meaning full index and printing the .loc results

--- Pandas-Assignment-Phase2/Task.py
+++ Pandas-Assignment-Phase2/Task.py
@@ -24,3 +24,10 @@ print(df[["Name","Passed","Category"]])
 print("-"*60)
 print("First Three column of the dataset : ")
 print(df.iloc[0:3])
+
+print("-"*60)
+df.index=[1001,1002,1003,1004,1005,1006,1007,1008]
+print("Printing name of the student by  id of student :-")
+print(df.loc[1004,"Name"])
+print("Printing the First three rows of Datasetv with updated index : " )
+print(df.loc[1001:1005])
