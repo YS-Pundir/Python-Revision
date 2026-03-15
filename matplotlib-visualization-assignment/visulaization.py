bf8959138d668971743b686ac252370b65ccac30
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-03-15 04:34:40 +0100

    adding the bar chart

--- matplotlib-visualization-assignment/visulaization.py
+++ matplotlib-visualization-assignment/visulaization.py
@@ -10,6 +10,7 @@ pip.title("Line Plot",color="red",fontweight="bold")
 pip.xlabel("Epochs",color="Blue",fontweight="bold")
 pip.ylabel("Loss value",color="Blue",fontweight="bold")
 pip.grid(True)
+pip.legend()
 pip.show()
 
 pip.figure()
@@ -17,5 +18,15 @@ pip.scatter(epochs,loss,marker="*")
 pip.title("Scatter Plot",color="red",fontweight="bold")
 pip.xlabel("Epochs",color="Blue",fontweight="bold")
 pip.ylabel("Loss value",color="Blue",fontweight="bold")
+pip.grid(True)
+pip.show()
+
+x=["Model A","Model B","Model C"]
+y=[0.85,0.90,0.88]
+pip.figure()
+pip.bar(x,y,color=["Red","Green","Blue"])
+pip.xlabel("Models",fontweight="bold",color="Black")
+pip.ylabel("Accuracy",fontweight="bold",color="Black")
+pip.title("Accuracy Bar Chart",color="red",fontweight="bold",fontsize=40)
 pip.grid(True)
 pip.show()
