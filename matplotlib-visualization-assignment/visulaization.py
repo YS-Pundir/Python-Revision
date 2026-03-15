7f501587116cbd3d68bd4bef1df06a4a90580651
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-03-15 04:22:17 +0100

    adding the line and scatter chart for assignment

--- matplotlib-visualization-assignment/visulaization.py
+++ matplotlib-visualization-assignment/visulaization.py
@@ -1,2 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as pip
+
+epochs=np.arange(1,11)
+loss=np.random.rand(10)
+
+pip.figure()
+pip.plot(epochs,loss,marker="s",linestyle=":",markerfacecolor="black")
+pip.title("Line Plot",color="red",fontweight="bold")
+pip.xlabel("Epochs",color="Blue",fontweight="bold")
+pip.ylabel("Loss value",color="Blue",fontweight="bold")
+pip.grid(True)
+pip.show()
+
+pip.figure()
+pip.scatter(epochs,loss,marker="*")
+pip.title("Scatter Plot",color="red",fontweight="bold")
+pip.xlabel("Epochs",color="Blue",fontweight="bold")
+pip.ylabel("Loss value",color="Blue",fontweight="bold")
+pip.grid(True)
+pip.show()
