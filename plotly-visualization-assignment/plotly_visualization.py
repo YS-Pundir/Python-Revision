e1e9622f1a36cbdce3d72dda1ea5f17ec83d8d52
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-03-16 19:57:35 +0100

    adding the chart svaed

--- plotly-visualization-assignment/plotly_visualization.py
+++ plotly-visualization-assignment/plotly_visualization.py
@@ -6,3 +6,23 @@ data=pd.DataFrame({
     "Loss":[0.99,0.78,0.65,0.23,0.56874,0.45,0.732,0.23,0.45,0.8479]
 })
 
+fig=px.line(
+    data,
+    x=data["Epoch"],
+    y=data["Loss"],
+    title="Training Loss Over Epochs"  
+
+)
+
+fig.add_annotation(
+    x=7,
+    y=0.732,
+    text="Main Point",
+    showarrow=True,
+    arrowhead=8,
+    arrowcolor="red"
+)
+
+
+fig.show()
+fig.write_html("Line_Char.png")
