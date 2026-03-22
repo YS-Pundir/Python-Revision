6e0aaff47e2a209dd2457143d42a25f2e370ebd4
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-03-22 20:22:10 +0100

    adding the identification of distribution and outleirs

--- EDA/eda-checklist/iris_eda.py
+++ EDA/eda-checklist/iris_eda.py
@@ -6,3 +6,38 @@ import plotly.express as px
 
 data=pd.read_csv("iris.csv")
 
+# Step 1 : Inspecting dataset Structure  using df.info() method of pandas libraris 
+print("The Datastructure of the Dataset is : ")
+print(data.info())
+print("The Statistical Summary of the Dataset is : ")
+print(data.describe())
+
+    # Observation : There are total  150 entries and 5 columns
+
+# Step 2 : Checking for column information and missing values using data.describe() and data.isnull().sum()
+print("-"*60)
+print("The Statistical Summary of the Dataset is : ")
+print(data.describe())
+print("Checking for the null values in dataset : ")
+print(data.isnull().sum())
+    # Obsevation : 4 out of 5 columns have numerical data , and there is no null values
+
+# Step 3 : Ananlysing Distribution of feature using the Histogram Chart
+fig=px.histogram(
+    data,
+    x="petal_length",
+    title="Distribution of Pental Length "
+)
+
+fig.show()
+    # Observation : The Dataset is highly dense bitween 1.3 and 1.7 values on pental length , and zero bitween 2.2 and 2.8
+
+# Step 4 : Identifing the Possible Outliers in dataset using the box Chart
+fig=px.box(
+    data,
+    y=["sepal_length","petal_length"],
+    title="Idnetifing the Possible outliers"
+)
+fig.show()
+    # Observation : The Dataset has no outliers for sepal_length,petal_length
+
